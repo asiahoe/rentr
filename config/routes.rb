0d1be8ef8e7449584a2aa3a26b66fe1d66d438da
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-17 14:17:34 -0500

    deleted extra lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 require "monban/constraints/signed_in"
 require "monban/constraints/signed_out"
 
-
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-
 
   constraints Monban::Constraints::SignedIn.new do
     root "sessions#new", as: :home
