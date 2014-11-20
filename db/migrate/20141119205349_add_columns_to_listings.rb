a037cacfb5687bc79affa5de2e8cffe05613e4b5
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-20 12:00:48 -0500

    put default: false to listing columns

--- db/migrate/20141119205349_add_columns_to_listings.rb
+++ db/migrate/20141119205349_add_columns_to_listings.rb
@@ -1,13 +1,13 @@
 class AddColumnsToListings < ActiveRecord::Migration
   def change
-    add_column :listings, :tv, :boolean
-    add_column :listings, :essentials, :boolean
-    add_column :listings, :shampoo, :boolean
-    add_column :listings, :heating, :boolean
-    add_column :listings, :conditioning, :boolean
-    add_column :listings, :washer, :boolean
-    add_column :listings, :indoor_fireplace, :boolean
-    add_column :listings, :dryer, :boolean
-    add_column :listings, :internet, :boolean
+    add_column :listings, :tv, :boolean, default: false
+    add_column :listings, :essentials, :boolean, default: false
+    add_column :listings, :shampoo, :boolean, default: false
+    add_column :listings, :heating, :boolean, default: false
+    add_column :listings, :conditioning, :boolean, default: false
+    add_column :listings, :washer, :boolean, default: false
+    add_column :listings, :indoor_fireplace, :boolean, default: false
+    add_column :listings, :dryer, :boolean, default: false
+    add_column :listings, :internet, :boolean, default: false
   end
 end
